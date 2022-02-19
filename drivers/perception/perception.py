d5995d715fc5f7282e58c28223cec55c7f6cc6f4
uraniumCoder <williamzhaohaitong7@gmail.com>
2022-02-19 17:41:53 -0500

    delete shit

--- drivers/perception/perception.py
+++ drivers/perception/perception.py
@@ -10,6 +10,20 @@ from queue import Queue
 sys.path.append('../../')
 from kernel_util import *
 
+import argparse
+import time
+from torch
+import numpy as np
+import cv2
+from CLIP import clip
+
+READ_EVERY = 5
+CAPTION_AFTER_FRAMES = 10
+MAX_CHUNK_FRAMES = 300
+DRIFT_SIMILARITY_THRESHOLD = 0.5
+NEW_CHUNK_SIMILARITY_THRESHOLD = 0.5
+
+clip_model, clip_preprocess = clip.load("ViT-L/14", device='cuda')
 
 class PerceptionDriver:
     buffer = Queue()
@@ -17,19 +31,41 @@ class PerceptionDriver:
     def __init__(self):
         pass
 
-    def next():
+    @classmethod
+    def push(cls, caption):
+        buffer.put(caption)
+
+    @classmethod
+    def next(cls):
         return buffer.get(block=True)
 
-    def reset():
+    @classmethod
+    def reset(cls):
         buffer = Queue()
 
 
 captions = ['The man is having a picnic in the park with his dog.']
 
 
-def embed_text(text):
-    return torch.randn(10)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Recording settings.')
 
+    parser.add_argument('-o', '--output_path', default='output.mp4', type=str,
+        help='')
+    parser.add_argument('-n', '--num_frames', default=2000, type=int,
+        help='')
+    parser.add_argument('--fps', default=24, type=int,
+        help='')
+
+    return parser.parse_args()
+
+def embed_texts(texts):
+    return clip_model.encode_text(texts)
+
+def embed_frame(frame):
+    tokens = clip_preprocess(frame)
+    embeddings = clip_model.encode_image(tokens)
+    return embeddings
 
 def embed_compare_func(clip_embedding):
     cosine_sim = nn.CosineSimilarity(dim=0)
@@ -37,7 +73,6 @@ def embed_compare_func(clip_embedding):
     def embed_compare(embedding):
         return cosine_sim(clip_embedding, embedding)
     return embed_compare
-
 
 def next_caption(clip_embedding):
     """ """
@@ -76,15 +111,117 @@ def next_caption(clip_embedding):
     20. The man will turn off the light and go to bed. """
     candidates = [x.strip('\n ') for x in re.split(r'[0-9]+. ', text)]
     candidates[0] = captions[-1]
-    embeddings = [embed_text(x) for x in candidates]
+    embeddings = embed_texts(candidates)
 
     scores = list(map(embed_compare_func(clip_embedding), embeddings))
     scores = torch.stack(scores)
     topidx = torch.argmax(scores).item()
-    return candidates[topidx]
+    return candidates[topidx], scores[topidx]
 
+def get_google_caption(embeds):
+    
 
 def clean_caption():
     pass
 
-#next_caption(torch.randn(10))
+def main():
+    args = parse_args()
+
+    video_cap = cv2.VideoCapture(0)
+    cap_prop = lambda x : int(video_cap.get(x))
+
+    width, height = \
+        cap_prop(cv2.CAP_PROP_FRAME_WIDTH), cap_prop(cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Camera dimensions: {}x{}".format(height, width))
+
+    start = time.time()
+    frames = []
+
+    last_segment_length = 0
+    last_segment_caption_embedding = None
+    sampling_counter = 0
+    begin_new_chunk = False
+    new_chunk_length = 0
+    new_chunk_tokens = []
+
+    while True:
+        success, frame = video_cap.read()
+        if not success or len(frames) > args.num_frames - 1:
+            break
+
+
+        """
+        Assume we're not currently beginning a new chunk.
+        We read a frame every READ_EVERY frames, and compute its embedding.
+        If its similarity to the last segment's caption is too low, or if 
+        the last segment has lasted at least MAX_CHUNK_FRAMES, we start a new chunk.
+        """
+
+        sampling_counter += 1
+        if sampling_counter % READ_EVERY == 0:
+            sampling_counter = 0
+            embedding = embed_frame(frame)
+
+            """
+            Detect whether we should start a new chunk. There're 3 cases:
+                1. This is the first frame
+                2. The last segment has been long enough
+                3. The last segment's caption is too dissimilar to the current frame
+            """
+            if last_segment_caption_embedding is None:
+                begin_new_chunk = True
+            elif last_segment_length > MAX_CHUNK_FRAMES:
+                begin_new_chunk = True
+            else:
+                similarity = clip.cosine_similarity(last_segment_caption_embedding, embedding)
+                if similarity < NEW_CHUNK_SIMILARITY_THRESHOLD:
+                    begin_new_chunk = False
+                    last_segment_length += READ_EVERY
+
+                else:
+                    begin_new_chunk = True
+
+        if begin_new_chunk:
+            """
+            We're currently beginning a new chunk.
+            We read every frame for the next CAPTION_AFTER_FRAMES frames,
+            then using the average embedding of the frames, we compute
+            a caption for the chunk.
+            """
+            new_chunk_length += 1
+            new_chunk_tokens.append(embed_frame(frame))
+            if new_chunk_length >= CAPTION_AFTER_FRAMES:
+                batch = torch.stack(new_chunk_tokens).to('cuda')
+                embeds = clip_model.encode_image(batch)
+                embeds = torch.mean(embeds, dim=0)
+
+                if last_segment_caption_embedding is not None:                    
+                    # Get the most similar caption using Bayesian factorization
+                    suggested_caption, similarity = next_caption(embeds)
+                    
+                    if similarity > DRIFT_SIMILARITY_THRESHOLD:
+                        caption = suggested_caption
+                    else:
+                        caption = get_google_caption(embeds)
+
+                else:
+                    caption = get_google_caption(embeds)
+
+                begin_new_chunk = False
+                new_chunk_length = 0
+                new_chunk_tokens = []
+                sampling_counter = 0
+                last_segment_length = CAPTION_AFTER_FRAMES
+                last_segment_caption_embedding = clip_model.encode_text(caption)
+                PerceptionDriver.push(caption)
+                captions.append(caption)
+
+
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            break
+
+    print ("Recording time taken : {0} seconds".format(time.time() - start))
+
+
+if __name__ == '__main__':
+    main()
